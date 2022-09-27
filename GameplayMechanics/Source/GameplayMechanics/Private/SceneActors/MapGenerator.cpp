e28b6a6f87758ba12e68ceb89e156d40786474ac
Sito <alfonsosanchez1896@gmail.com>
2022-09-27 10:47:42 +0200

    Poison disk sampling algorithm

--- GameplayMechanics/Source/GameplayMechanics/Private/SceneActors/MapGenerator.cpp
+++ GameplayMechanics/Source/GameplayMechanics/Private/SceneActors/MapGenerator.cpp
@@ -1,39 +1,235 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SceneActors/MapGenerator.h"
-#include "Math/UnrealMathUtility.h"
-#include "Components/StaticMeshComponent.h"
-#include "Materials/MaterialInstanceDynamic.h"
+#include "DrawDebugHelpers.h"
+#include "GenericPlatform/GenericPlatformMath.h"
+
 // Sets default values
 AMapGenerator::AMapGenerator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//DefaultActor = CreateDefaultSubobject<AActor>(TEXT("Default Node Actor"));
-
-	MapLayers = 15;
-	MapDensity = 7;
-	MaxMapConections = 4;
-	DistanceBetweenNodes = 50;
-	DistanceBetweenLayers = 100;
-
-	ProbabilityNormalBattle = 0.2f;
-	ProbabilityBossBattle = 0.05f;
-	ProbabilityEventRoom = 0.1f;
-	ProbabilityRewardRoom = 0.1;
-	ProbabilityShoopRoom = 0.1;
-	ProbabilityNoRoom = 0.3;
+	StartPoint = FVector(0.0f);
+	EndPoint = FVector(50.0f, 0.0f, 0.0f);
+	HalfGridWeight = 25.f;
+	SphereRadius = 5.0f;
+	NumSampleBeforeRejection = 1;
+	Seed = 123456789;
+	Iterations = 50000;
 }
 
 // Called when the game starts or when spawned
 void AMapGenerator::BeginPlay()
 {
 	Super::BeginPlay();
+	MyPoisonDiskSamplingAlgorithm();
+}
+
+bool AMapGenerator::IsMyCandidateValid(FVector2D Candidate, FVector2D RegionSize, float CellSize, TArray<int> GridCells)
+{
+	if (Candidate.X >= 0 && Candidate.X < RegionSize.X && Candidate.Y >= 0 && Candidate.Y < RegionSize.Y)
+	{
+		int LocationX = (int)(Candidate.X / CellSize);
+		int LocationY = (int)(Candidate.Y / CellSize);
+
+		//To search 5 by 5 around the cell
+		int SearchStartX = FMath::Max(0, LocationX - 2);
+		int SearchEndX = FMath::Min(LocationX + 2, RegionSize.X/ CellSize);
+
+		int SearchStartY = FMath::Max(0, LocationY - 2);
+		int SearchEndY = FMath::Min(LocationY + 2, RegionSize.Y/ CellSize);
+
+		for (int X = SearchStartX; X <= SearchEndX; ++X)
+		{
+			for (int Y = SearchStartY; Y <= SearchEndY; ++Y)
+			{
+				int Index = ceil(RegionSize.X/CellSize) * X + Y;
+				int PointIndex = GridCells[Index] - 1;
+
+				if (PointIndex != -1)
+				{
+					FVector2D Point = GeneratedPoints[PointIndex];
+					FVector2D Dist = Candidate - Point;
+					float SqrtDistance = Dist.SizeSquared();
+
+					if (SqrtDistance < SphereRadius * SphereRadius)
+					{
+						return false;
+					}
+
+				}
+
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+
+void AMapGenerator::MyPoisonDiskSamplingAlgorithm()
+{
+	GeneratedPoints.Reset(0);
+	Grid.Reset(0);
+
+	FRandomStream Random = FRandomStream(Seed);
+	float PISimplified = 3.141592654f;
+
+	FVector2D RegionSize = FVector2D(EndPoint.X - StartPoint.X, HalfGridWeight * 2.f);
+	float CellSize = SphereRadius / FMath::Sqrt(2.0f);
+	int MaxGridCellsX = ceil(RegionSize.X/CellSize);
+	int MaxGridCellsY = ceil(RegionSize.Y/CellSize);
+	int GridSize = MaxGridCellsX * MaxGridCellsY;
+
 	
-	GenerateBaseMap();
-	GenerateRandomRooms();
+	Grid.SetNumZeroed(GridSize);
+
+	TArray<FVector2D> SpawnPoints;
+
+	//StartedPoint, we try with the middle point
+	FVector2D StartedPoint = RegionSize / 2.0f;
+	SpawnPoints.Add(StartedPoint);
+
+	while (SpawnPoints.Num() > 0 && Iterations > 0)
+	{
+		int RandomSpawnIndex = Random.RandRange(0, SpawnPoints.Num() - 1);
+		FVector2D SpawnRandomPoint = SpawnPoints[RandomSpawnIndex];
+
+		bool bCandidateAccepted = false;
+
+		for (int Index = 0; Index < NumSampleBeforeRejection; ++Index)
+		{
+			float Angle = Random.FRand() * PISimplified * 2;
+			FVector2D Direction = FVector2D(FMath::Sin(Angle), FMath::Cos(Angle));
+			FVector2D CandidatePoint = SpawnRandomPoint + Direction * Random.RandRange(SphereRadius, SphereRadius * 2.0f);
+
+			if (IsMyCandidateValid(CandidatePoint, RegionSize, CellSize, Grid))
+			{
+				GeneratedPoints.Add(CandidatePoint);
+				SpawnPoints.Add(CandidatePoint);
+
+				int LocationX = (int)(CandidatePoint.X / CellSize);
+				int LocationY = (int)(CandidatePoint.Y / CellSize);
+
+				int GridIndex = MaxGridCellsX * LocationX + LocationY;
+				Grid[GridIndex] = GeneratedPoints.Num();
+				bCandidateAccepted = true;
+				break;
+			}
+
+		}
+
+		if (!bCandidateAccepted)
+		{
+			SpawnPoints.RemoveAt(RandomSpawnIndex);
+		}
+		Iterations -= 1;
+	}
+
+	if (!SpawnPoints.IsEmpty() && SpawnPoints[0] == StartedPoint)
+	{
+		SpawnPoints.RemoveAt(0);
+	}
+
+	for (int Index = 0; Index < SpawnPoints.Num() - 2; ++Index)
+	{
+		FVector2D Pos = SpawnPoints[Index];
+
+		for (int NextIndex = Index + 1; NextIndex < SpawnPoints.Num(); ++NextIndex)
+		{
+			FVector2D NextPos = SpawnPoints[NextIndex];
+
+			FVector2D Dist = NextPos - Pos;
+			float SqrtDistance = Dist.SizeSquared();
+
+			if (SqrtDistance < SphereRadius * SphereRadius)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Bad Disck Noise Sample"));
+			}
+		}
+	}
+}
+
+void AMapGenerator::DrawDebugStartEndPoints()
+{
+	UWorld* World = GetWorld();
+	DrawDebugSphere(World, StartPoint, 1.0f, 10, FColor::Green);
+	DrawDebugSphere(World, EndPoint, 1.0f, 10, FColor::Blue);
+}
+
+void AMapGenerator::DrawDebugGrid()
+{
+	UWorld* World = GetWorld();
+	FVector BoxCenter = EndPoint - StartPoint;
+
+	if (StartPoint.IsZero())
+	{
+		BoxCenter /= 2.0f;
+	}
+
+	DrawDebugBox(World, BoxCenter, FVector(BoxCenter.X, HalfGridWeight * 2.f, 0.1f), FColor::Green);
+
+	FVector2D RegionSize = FVector2D(EndPoint.X - StartPoint.X, HalfGridWeight * 2.f);
+	float CellSize = SphereRadius / FMath::Sqrt(2.0f);
+	int MaxGridCellsX = ceil(RegionSize.X / CellSize);
+	int MaxGridCellsY = ceil(RegionSize.Y / CellSize);
+
+	for (int Index = 0; Index < MaxGridCellsX; ++Index)
+	{
+		FVector Start = StartPoint;
+		Start.Y = Index*CellSize;
+		FVector End = EndPoint;
+		End.Y = Index * CellSize;
+		DrawDebugLine(World, Start, End, FColor::Black);
+	}
+
+	for (int Index = 0; Index < MaxGridCellsY; ++Index)
+	{
+		FVector Start = FVector(0.0f);
+		Start.X = Index * CellSize;
+		FVector End = FVector(0.0f,RegionSize.Y,0.0f);
+		End.X = Index * CellSize;
+		DrawDebugLine(World, Start, End, FColor::Black);
+	}
+
+	for (int X = 0; X < MaxGridCellsX; ++X)
+	{
+		for (int Y = 0; Y < MaxGridCellsY; ++Y)
+		{
+			int GridIndex = MaxGridCellsX * X + Y;
+
+			FColor Color = FColor::Black;
+
+			if (Grid[GridIndex] - 1 != -1)
+			{
+				Color = FColor::Magenta;
+			}
+
+			FVector Position = FVector(X * CellSize + CellSize/2.0f, Y * CellSize + CellSize / 2.0f, 0.0f);
+
+			DrawDebugSphere(World, Position, 0.2, 1.0f, Color);
+		}
+	}
+
+}
+
+void AMapGenerator::DrawDebugPoisonDisk()
+{
+	DrawDebugGrid();
+	DrawDebugStartEndPoints();
+	
+	UWorld* World = GetWorld();
+
+	for (int Index = 0; Index < GeneratedPoints.Num(); ++Index)
+	{
+		FVector Position = FVector(GeneratedPoints[Index].X, GeneratedPoints[Index].Y, 0.0f);
+		DrawDebugSphere(World, Position, SphereRadius, 10, FColor::Red);
+		DrawDebugSphere(World, Position, 0.1, 10, FColor::Blue);
+	}
+
 }
 
 
@@ -41,95 +237,5 @@ void AMapGenerator::BeginPlay()
 void AMapGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-}
-
-void AMapGenerator::GenerateBaseMap()
-{
-	const int MaxNodes = MapLayers * MapDensity;
-	MapArray.Init(BaseNode, MaxNodes);
-
-	UWorld* World = GetWorld();
-
-	for (int Index = 0; Index < MaxNodes; ++Index)
-	{
-		const int Column = Index / MapLayers;
-		FVector Location(0.f,0.f,0.f);
-
-		Location.X = ((Index + MapLayers * Column) % MapLayers) * DistanceBetweenNodes;
-		Location.Y = ((Index + MapLayers * Column) / MapLayers) * DistanceBetweenLayers;
-
-		FRotator Rotator = FRotator::ZeroRotator;
-		FActorSpawnParameters SpawnParameters;
-
-		MapArray[Index].NodeActor = World->SpawnActor<AActor>(DefaultActor, Location, Rotator);
-	}
-}
-
-void AMapGenerator::GenerateRandomRooms()
-{
-	const float MaxProbabilities = ProbabilityNormalBattle + ProbabilityBossBattle + ProbabilityEventRoom
-		+ ProbabilityRewardRoom + ProbabilityShoopRoom + ProbabilityNoRoom;
-
-	const int MaxNodes = MapLayers * MapDensity;
-
-	float MaxProbabilityBossBattle = ProbabilityBossBattle + ProbabilityNormalBattle;
-	float MaxProbabilityEventRoom = MaxProbabilityBossBattle + ProbabilityEventRoom;
-	float MaxProbabilityRewardRoom = ProbabilityEventRoom + ProbabilityRewardRoom;
-	float MaxProbabilityShoopRoom = MaxProbabilityRewardRoom + ProbabilityShoopRoom;;
-
-	float RandomValue = 0.0f;
-
-	for (int Index = 0; Index < MaxNodes; ++Index)
-	{
-		UStaticMeshComponent* StaticMesh = Cast<UStaticMeshComponent>(MapArray[Index].NodeActor->GetComponentByClass(UStaticMeshComponent::StaticClass()));
-
-		if (StaticMesh != nullptr)
-		{
-			RandomValue = FMath::RandRange(0.f, MaxProbabilities);
-			
-			UMaterialInstanceDynamic* Mat = UMaterialInstanceDynamic::Create(StaticMesh->GetMaterial(0), nullptr);
-			
-			if (Mat != nullptr)
-			{
-				if (RandomValue < ProbabilityNormalBattle) //Normal Battle
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(0.0f, 1.0f, 0.0f, 1.0f));
-					MapArray[Index].Node = NodeType::NormalBattle;
-				}
-				else if (RandomValue >= ProbabilityNormalBattle && RandomValue < MaxProbabilityBossBattle) //Boss Battle
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(1.0f, 0.0f, 0.0f, 1.0f));
-					MapArray[Index].Node = NodeType::BossBattle;
-				}
-				else if (RandomValue >= MaxProbabilityBossBattle && RandomValue < MaxProbabilityEventRoom) //Event Room
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(0.0f, 0.0f, 1.0f, 1.0f));
-					MapArray[Index].Node = NodeType::EventRoom;
-				}
-				else if (RandomValue >= MaxProbabilityEventRoom && RandomValue < MaxProbabilityRewardRoom) //Reward Room
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(0.5f, 0.5f, 0.1f, 1.0f));
-					MapArray[Index].Node = NodeType::RewardRoom;
-				}
-				else if (RandomValue >= MaxProbabilityRewardRoom && RandomValue < MaxProbabilityShoopRoom) //Shoop Room
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(1.0f, 1.0f, 1.0f, 1.0f));
-					MapArray[Index].Node = NodeType::ShopRoom;
-				}
-				else //No Room
-				{
-					Mat->SetVectorParameterValue("Base Color", FVector4(0.0f, 0.0f, 0.0f, 1.0f));
-				}
-
-				StaticMesh->SetMaterial(0, Mat);
-
-			}
-		}
-	}
-}
-
-FMapNode::FMapNode()
-{
-	NodeActor = nullptr;
+	DrawDebugPoisonDisk();
 }
--- GameplayMechanics/Source/GameplayMechanics/Public/SceneActors/MapGenerator.h
+++ GameplayMechanics/Source/GameplayMechanics/Public/SceneActors/MapGenerator.h
@@ -6,33 +6,6 @@
 #include "GameFramework/Actor.h"
 #include "MapGenerator.generated.h"
 
-UENUM()
-enum NodeType
-{
-	NormalBattle = 0,
-	BossBattle,
-	FinalBattle,
-	EventRoom,
-	ShopRoom,
-	RewardRoom,
-	Default
-};
-
-
-USTRUCT()
-struct FMapNode
-{
-	GENERATED_BODY()
-
-	FMapNode();
-
-
-public:
-	TEnumAsByte<NodeType> Node = NodeType::Default;
-	AActor* NodeActor;
-};
-
-
 UCLASS()
 class GAMEPLAYMECHANICS_API AMapGenerator : public AActor
 {
@@ -45,54 +18,46 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-private:
 	
-	void GenerateBaseMap();
-	void GenerateRandomRooms();
+private:
 
+	void PoisonDiskSamplingAlgorithm();
+	bool IsCandidateValid(FVector2D Candidate, FVector2D SampleRegionSize, float CellSize, TArray<int> Grid);
+
+	void MyPoisonDiskSamplingAlgorithm();
+	bool IsMyCandidateValid(FVector2D Candidate, FVector2D SampleRegionSize, float CellSize, TArray<int> Grid);
+
+	void DrawDebugStartEndPoints();
+	void DrawDebugGrid();
+	void DrawDebugPoisonDisk();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	int MapLayers;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	int MapDensity;
+	UPROPERTY(EditAnywhere)
+	FVector StartPoint;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	int MaxMapConections;
+	UPROPERTY(EditAnywhere)
+	FVector EndPoint;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	int DistanceBetweenNodes;
+	UPROPERTY(EditAnywhere)
+	float HalfGridWeight;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	int DistanceBetweenLayers;
+	UPROPERTY(EditAnywhere)
+	int NumSampleBeforeRejection;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Generator Settings")
-	TSubclassOf<AActor> DefaultActor;
+	UPROPERTY(EditAnywhere)
+	float SphereRadius;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityNormalBattle;
+	UPROPERTY(EditAnywhere)
+	int Seed;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityBossBattle;
+	UPROPERTY(EditAnywhere)
+	int Iterations;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityEventRoom;
+	TArray<FVector2D> GeneratedPoints;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityShoopRoom;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityRewardRoom;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Room Probability", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float ProbabilityNoRoom;
-
-private:
-
-	TArray<FMapNode> MapArray;
-	FMapNode BaseNode;
+	TArray<int> Grid;
 };
+
